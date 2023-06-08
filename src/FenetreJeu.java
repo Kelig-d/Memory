23ac29f4579e07baab97be507530cbbd777567f4
bheard <bheard@enssat.fr>
2023-06-08 10:57:22 +0200

    modified:   src/FenetreJeu.java 	modified:   src/Modele.java 	new file:   src/Observer.java 	new file:   src/Subject.java

--- src/FenetreJeu.java
+++ src/FenetreJeu.java
@@ -1,3 +1,7 @@
 public class FenetreJeu {
+
+    // Attributs
+    private Controleur control;
+	private Modele model;
     
 }
--- src/Modele.java
+++ src/Modele.java
@@ -3,38 +3,12 @@ import java.util.List;
 
 public class Modele implements Subject
 {
+    private List<Carte> cartes = new ArrayList<>()
 	private List<Observer> observers = new ArrayList<>();
 	 
 	public Modele()
 	{
-		Employe emp;
-		
-		this.liste = new ArrayList<Employe>();
-		
-		emp = new Employe(1,"Robert","Dupont","Comptabilit�");
-		this.liste.add(emp);
-		
-		emp = new Employe(2,"Roger","Durand","Comptabilit�");
-		this.liste.add(emp);
-	
-		emp = new Employe(3,"Anne-Marie","Siramis","Secr�tariat");
-		this.liste.add(emp);
-		
-		emp = new Employe(4,"Marc-Andr�","Fouchard","Secr�tariat");
-		this.liste.add(emp);
-		
-		emp = new Employe(5,"Laurent","Chandez","Vente");
-		this.liste.add(emp);
-	
-		emp = new Employe(6,"Fr�d�ric","Caule","Vente");
-		this.liste.add(emp);
-		
-		emp = new Employe(7,"Annabelle","Petitalot","Vente");
-		this.liste.add(emp);
-		
-		emp = new Employe(8,"V�ronique","Durantin","Direction");
-		this.liste.add(emp);
-	}
+    }
 	
 	public List<Employe> getList()
 	{  
