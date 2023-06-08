23b056dbb56d549efb49298fa576bf84dbcda51b
bheard <bheard@enssat.fr>
2023-06-08 11:39:18 +0200

    new file:   Images/Dos0.png 	modified:   src/Modele.java

--- src/Modele.java
+++ src/Modele.java
@@ -1,6 +1,7 @@
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Random;
 
 import javax.swing.JLabel;
 
@@ -9,19 +10,25 @@ public class Modele implements Subject
     private List<Carte> liste;
 	private List<Observer> observers = new ArrayList<>();
 
+    private double dos;
+
 	private List<String> themes = new ArrayList<>(Arrays.asList(new String[]{"theme 1", "theme 2"}));
 	private List<String> sizes = new ArrayList<>(Arrays.asList(new String[]{"4x3", "4x4", "5x4", "6x5", "6x6", "7x6"}));
+
+    private int nb_dos = 1;
+    private int nb_face = 0;
 
 	public Modele()
 	{		
 		this.liste = new ArrayList<Carte>();
+        this.dos = (Math.random())%((double)(this.nb_dos));
     }
-    
+
     public void creerCartes(int nbCartes){
         Carte carte;
         for(int i=0; i<nbCartes; i+=2){
             // A compléter
-            carte = new Carte(i, i+1, null, null);
+            carte = new Carte(i, i+1, null, );
             this.liste.add(carte);
             // A compléter
             carte = new Carte(i+1, i, null, null);
