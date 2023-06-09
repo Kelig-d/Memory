e85476fb3f0cde4e607a6543b6c890c648a3662e
bheard <bheard@enssat.fr>
2023-06-09 11:39:36 +0200

    new file:   Images/Flowers/face6.png 	new file:   Images/Flowers/face7.png 	new file:   Images/Flowers/face8.png 	new file:   Images/Flowers/face9.png 	modified:   src/Modele.java

--- src/Modele.java
+++ src/Modele.java
@@ -17,7 +17,7 @@ public class Modele implements Subject
 	private List<String> sizes = new ArrayList<>(Arrays.asList(new String[]{"4x3", "4x4", "5x4", "6x5", "6x6", "7x6"}));
 
     private int nb_dos = 1;
-    private int nb_face = 6;
+    private int nb_face_flowers = 9;
 
 	private String firstPlayer;
 	private String secondPlayer;
@@ -33,11 +33,11 @@ public class Modele implements Subject
 
     public void creerCartes(int nbCartes){
         Carte carte;
-		int Num_face = ((int) Math.random())%((this.nb_face));
+		int Num_face = ((int) Math.random())%((this.nb_face_flowers));
         for(int i=0; i<nbCartes; i+=2){
             // A compléter
 
-			Icon face = new ImageIcon("Images/Flowers/face" + Num_face%this.nb_face +".png");
+			Icon face = new ImageIcon("Images/Flowers/face" + Num_face%this.nb_face_flowers +".png");
             carte = new Carte(i, i+1, face, dos);
             this.liste.add(carte);
             // A compléter
