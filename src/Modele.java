59323502c23f53ae3db0293bdf7b1658d219f314
bheard <bheard@enssat.fr>
2023-06-09 11:04:52 +0200

    modified:   src/Modele.java

--- src/Modele.java
+++ src/Modele.java
@@ -4,7 +4,6 @@ import java.util.List;
 
 import javax.swing.Icon;
 import javax.swing.ImageIcon;
-import javax.swing.JLabel;
 
 public class Modele implements Subject
 {
@@ -18,7 +17,7 @@ public class Modele implements Subject
 	private List<String> sizes = new ArrayList<>(Arrays.asList(new String[]{"4x3", "4x4", "5x4", "6x5", "6x6", "7x6"}));
 
     private int nb_dos = 1;
-    private int nb_face = 0;
+    private int nb_face = 6;
 
 	private String firstPlayer;
 	private String secondPlayer;
@@ -36,10 +35,12 @@ public class Modele implements Subject
         Carte carte;
         for(int i=0; i<nbCartes; i+=2){
             // A compléter
-            carte = new Carte(i, i+1, null, dos);
+			int Num_face = ((int) Math.random())%((this.nb_face));
+			Icon face = new ImageIcon("Images/Flowers/Face" + Num_face +".png");
+            carte = new Carte(i, i+1, face, dos);
             this.liste.add(carte);
             // A compléter
-            carte = new Carte(i+1, i, null, dos);
+            carte = new Carte(i+1, i, face, dos);
             this.liste.add(carte);
         }
     }
