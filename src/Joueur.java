82a43783a68aad46e0b3087d6320b20a4d8849eb
bheard <bheard@enssat.fr>
2023-06-14 15:09:12 +0200

    new file:   src/Joueur.java

---
+++ src/Joueur.java
@@ -0,0 +1,49 @@
+public class Joueur {
+
+    //attributs
+    private String nom;
+    private int score = 0;
+    private Boolean jouer;
+    
+    //Constructeur
+    public Joueur(String nom, int score, Boolean jouer){
+        this.nom = nom;
+        this.score = score;
+        this.jouer= jouer;
+    }
+
+    //méthode
+    public void incrementerScore(){
+        this.score += 1;
+    }
+
+    public void changerJoueur(){
+        this.jouer = !(this.jouer);
+    }
+
+    //setter
+    public void setNom(String nom){
+        this.nom = nom;
+    }
+
+    public void setScore(int score){
+        this.score = score;
+    }
+
+    public void setJouer(Boolean jouer){
+        this.jouer = jouer;
+    }
+
+    //getter
+    public String getNom(){
+        return this.nom;
+    }
+
+    public int getScore(){
+        return this.score;
+    }
+
+    public Boolean getJouer(){
+        return this.jouer;
+    }
+}
